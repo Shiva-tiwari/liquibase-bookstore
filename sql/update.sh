fd64eff2572a4f7f048258ff4491241ec9baa8e6
Shiva <shiva.tiwari@tothenew.com>
2023-07-05 15:52:45 +0530

    using liquibase cmd line to make db updates

---
+++ sql/update.sh
@@ -0,0 +1,1 @@
+liquibase --driver=com.mysql.cj.jdbc.Driver --classpath="/Users/shivatiwari/Downloads/mysql-connector-j-8.0.33/mysql-connector-j-8.0.33.jar" --changeLogFile=db-changelog.xml --url="jdbc:mysql://localhost:3306/bookstore" --username=root --password= update
